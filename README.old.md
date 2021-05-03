1c3652c46febe8cd09bccfa44c4c0580f6699cfc
charburgx <charburgx@gmail.com>
2021-05-03 19:25:37 -0400

    Initial commit

---
+++ README.old.md
@@ -0,0 +1,1 @@
+# flplus Website
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.scss';
+import 'materialize-css';
+import { Button } from 'react-materialize';
+
+const App = () => {
+  return (
+    <div className="App">
+      <Button>Test</Button>
+    </div>
+  );
+}
+
+export default App;
